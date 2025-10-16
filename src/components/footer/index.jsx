ee713573b6f02e8fc37c5b68db58aab84cd874fa
Estefano Ströher <estefanosoares9@gmail.com>
2025-10-16 19:27:42 -0300

    first commit

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="footer-container">
+            <p>© 2024 Minha Loja. Todos os direitos reservados.</p>
+        </footer>
+    );
+};
+
+export default Footer;
