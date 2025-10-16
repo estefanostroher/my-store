19fa80459d1ecea485ad36794aa7a391e1beba25
AntonniSMoraes <antonnimoraes@gmail.com>
2025-10-16 19:44:16 -0300

    header done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
 import Footer from './components/footer'
+import Header from './components/header'
 
 function App() {
 
   return (
     <>
+      <Header />
       <Footer />
     </>
   )
