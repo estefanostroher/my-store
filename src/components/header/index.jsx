f013ec7a7b21ee864d83cbffdaca5ca657218a30
AntonniSMoraes <antonnimoraes@gmail.com>
2025-10-21 21:18:41 -0300

    adjusts bootstrap header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
     return (
-        <header className="d-flex align-items-center header-container">
+        <header className="d-flex align-items-center header-container  justify-content-center bg-primary color-white text-white">
             <h1>Minha Loja</h1>
         </header>
     )
